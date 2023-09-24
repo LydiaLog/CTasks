4393b9cf2781d0160bad1530ac35b766cb86c0e2
Lydia.Y <110841277+LydiaLog@users.noreply.github.com>
2023-09-24 22:14:59 +0900

    week02
    2주차 과제 풀이

---
+++ No.5/printCost.c
@@ -0,0 +1,18 @@
+/* 실습 4 : printCost.c
+   작성일 : 2022.03.11
+   작성자 : 윤서은
+   프로그램 설명 : cost값을 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	double cost = 0.0;
+	
+	cost = 172.53;
+	
+	printf("The sales total is : $\t %f", cost);
+
+	return 0;
+}
